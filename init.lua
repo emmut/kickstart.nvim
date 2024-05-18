e35bb1039c9e9cc19914027973f4cf4ea010e326
Emil Jansson <emil.jansson@compileit.com>
2024-05-18 23:17:17 +0200

    🔧 use tokyonight color scheme

--- init.lua
+++ init.lua
@@ -911,4 +911,4 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme 'tokyonight-night'
